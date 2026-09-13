3743f2ce02a99649443b80b9d0da107aff5a3475
Machong Paul <59781024+sirpolux@users.noreply.github.com>
2026-09-13 21:58:21 +0100

    update

--- src/main/java/com/project/fintrack2/account/dto/AccountRequestDto.java
+++ src/main/java/com/project/fintrack2/account/dto/AccountRequestDto.java
@@ -1,4 +1,29 @@
 package com.project.fintrack2.account.dto;
 
+import com.project.fintrack2.Status;
+import com.project.fintrack2.account.enums.AccountType;
+import com.project.fintrack2.account.model.Currency;
+import com.project.fintrack2.user.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class AccountRequestDto {
+    private Long id;
+    private User user_id;
+    private String accountName;
+    private AccountType accountType;
+    private Currency currency;
+    private Float accountBalance;
+    private Status status;
+    private LocalDate dateCreated;
+    private LocalDate lastUpdated;
 }
