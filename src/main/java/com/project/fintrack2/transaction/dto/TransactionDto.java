6ad0259a053f447cda674f07918a7999281fb3d9
Machong Paul <poluxreloaded@gmail.com>
2026-09-18 23:44:34 +0100

    update

---
+++ src/main/java/com/project/fintrack2/transaction/dto/TransactionDto.java
@@ -0,0 +1,31 @@
+package com.project.fintrack2.transaction.dto;
+
+import com.project.fintrack2.account.model.Account;
+import com.project.fintrack2.categories.model.Category;
+import com.project.fintrack2.transaction.enums.TransactionStatus;
+import com.project.fintrack2.user.model.User;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TransactionDto {
+    private Long id;
+    private String transactionReference;
+    private Category category;
+    private User userId;
+    private Account accountId;
+    private String currencyCode;
+    private BigDecimal amount;
+    private String description;
+    private LocalDate transactionDate;
+    private TransactionStatus status;
+    private String reference; //for external transaction reference
+    private LocalDate createdAt;
+
+}
