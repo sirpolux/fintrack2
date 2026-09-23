d20dba4b8849ab591bbce986e8de1665eaab9819
Machong Paul <59781024+sirpolux@users.noreply.github.com>
2026-09-23 22:22:18 +0100

    added flow for adding role

--- src/main/java/com/project/fintrack2/user/repo/RoleRepository.java
+++ src/main/java/com/project/fintrack2/user/repo/RoleRepository.java
@@ -2,11 +2,13 @@ package com.project.fintrack2.user.repo;
 
 import com.project.fintrack2.user.dto.request.RoleRequestDto;
 import com.project.fintrack2.user.dto.response.RoleResponseDto;
+import com.project.fintrack2.user.model.Role;
 import com.project.fintrack2.utility.response.ResponseWrapper;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RoleRepository {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
 
 }
--- src/main/java/com/project/fintrack2/user/services/RoleService.java
+++ src/main/java/com/project/fintrack2/user/services/RoleService.java
@@ -1,23 +1,32 @@
-package com.project.fintrack2.user.services.contract;
+package com.project.fintrack2.user.services;
 
+import com.project.fintrack2.auth.AuthenticationContract;
 import com.project.fintrack2.dto.PaginationRequestDto;
+import com.project.fintrack2.mapper.RoleMapper;
 import com.project.fintrack2.user.dto.request.RoleRequestDto;
 import com.project.fintrack2.user.dto.response.RoleResponseDto;
+import com.project.fintrack2.user.model.Role;
+import com.project.fintrack2.user.model.User;
 import com.project.fintrack2.user.repo.RoleRepository;
+import com.project.fintrack2.user.services.contract.RoleServiceInterface;
 import com.project.fintrack2.utility.response.ResponseWrapper;
-import com.sun.security.auth.UserPrincipal;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
+import org.springframework.http.HttpStatus;
 
 
 @RequiredArgsConstructor
-public class RoleService implements RoleServiceInterface{
+public class RoleService implements RoleServiceInterface {
 
+    private final AuthenticationContract auth;
     private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
     @Override
     public ResponseWrapper<RoleResponseDto> save(RoleRequestDto requestDto) {
-
-        return null;
+        User user = auth.getAuthenticatedUser();
+        Role role = RoleMapper.toRole(requestDto);
+        role.setCreatedBy(user);
+        Role createdRole = roleRepository.save(role);
+        return ResponseWrapper.success("Role created",RoleMapper.toRoleResponseDto(createdRole), HttpStatus.CREATED);
     }
 
     @Override
