16838b859325a337bac2a11ae6a46b3849674846
Machong Paul <59781024+sirpolux@users.noreply.github.com>
2026-09-23 22:00:40 +0100

    update

--- src/main/java/com/project/fintrack2/user/repo/RoleRepository.java
+++ src/main/java/com/project/fintrack2/user/repo/RoleRepository.java
@@ -4,7 +4,11 @@ import com.project.fintrack2.user.dto.request.RoleRequestDto;
 import com.project.fintrack2.user.dto.response.RoleResponseDto;
 import com.project.fintrack2.utility.response.ResponseWrapper;
 
-public interface RoleRepository {
+import java.util.List;
 
+public interface RoleRepository {
+        ResponseWrapper<RoleResponseDto> save(RoleRequestDto roleRequestDto);
+        ResponseWrapper<List<RoleResponseDto>> getAll();
+        ResponseWrapper<RoleResponseDto> update(RoleRequestDto roleRequestDto);
 
 }
